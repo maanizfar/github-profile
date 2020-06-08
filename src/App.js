355364b351eddd59065caa8f185c4250503b5348
Salman Zafar <maanizfar@gmail.com>
2020-06-08 19:50:29 +0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Switch>
+      <Route path="/:username" component={Profile} />
+      <Route path="/" component={Home} />
+    </Switch>
+  );
 }
 
 export default App;
