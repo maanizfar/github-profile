1a5e66d343f9cd5d7add9bfe366103a0e18973b4
Salman Zafar <maanizfar@gmail.com>
2020-06-09 23:09:00 +0500

    fixed import warnings

--- src/components/UserInfoSection.js
+++ src/components/UserInfoSection.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import Octicon, { Location, Calendar, Briefcase } from "@primer/octicons-react";
 import { defaultTheme } from "../styles/theme";
